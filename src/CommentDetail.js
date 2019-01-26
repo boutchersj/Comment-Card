b7cbb397e5ba4a6a8752a87dd2987be1f6c91551
Steven Boutcher <sjboutcher@gmail.com>
2019-01-26 10:58:56 -0600

    Update CommentDetail.js

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = props => {
-  return (
-    <div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={props.avatar} />
-        </a>
-      </div>
-      <div className="content">
-        <a href="/" className="author">
-          {props.author}
-        </a>
-        <div className="date-metadata">
-          <span className="date">{props.timeAgo}</span>
+class CommentDetail extends React.Component {
+  render() {
+    return(
+      <div>
+        <div className="comment">
+          <a href="/" className="avatar">
+          <img alt="avatar" src={this.props.avatar} />
+          </a>
         </div>
-        <div className="text">{props.comment}</div>
+        <div className="content">
+          <a href="/" className="author">
+            {this.props.author}
+          </a>
+          <div className="date-metadata">
+            <span className="date">{this.props.timeAgo}</span>
+          </div>
+          <div className="text">{this.props.message}</div>
+        </div>
       </div>
-    </div>
-  );
-};
+    )
+  }
+}
 
 export default CommentDetail;
